4035a387043b9f696fb3974ba62399fac5c38c19
Paras Arya <parasarya289@gmail.com>
2023-11-19 19:28:21 +0530

    style

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import 'react-date-range/dist/styles.css'; // main style file
-import 'react-date-range/dist/theme/default.css'; // theme css file
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
 import "./index.css";
 import App from "./App";
 import { AuthContextProvider } from "./Context/AuthContext";
 import { DataContextProvider } from "./Context/DataContext";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <DataContextProvider>
-        <App />
-      </DataContextProvider>
-    </AuthContextProvider>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <DataContextProvider>
+          <App />
+        </DataContextProvider>
+      </AuthContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
