67a36c0997db3855757960ab20b8ffbacc6a4292
Paras Arya <parasarya289@gmail.com>
2023-11-19 19:34:26 +0530

    style: style changes

--- src/Components/BarChartComponent.js
+++ src/Components/BarChartComponent.js
@@ -9,7 +9,7 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts";
-export const BarChartComponent = ({ data,setCategory }) => {
+export const BarChartComponent = ({ data, setCategory }) => {
   return (
     <ResponsiveContainer width="100%" height={300}>
       <BarChart data={data}>
@@ -18,12 +18,42 @@ export const BarChartComponent = ({ data,setCategory }) => {
         <YAxis />
         <Tooltip />
         <Legend />
-        <Bar dataKey="a" fill="#8a2be2"  onClick={()=>setCategory("a")}/>
-        <Bar dataKey="b" fill="#20b2aa"  onClick={()=>setCategory("b")}/>
-        <Bar dataKey="c" fill="#ff4500"  onClick={()=>setCategory("c")}/>
-        <Bar dataKey="d" fill="#1e90ff" onClick={()=>setCategory("d")} />
-        <Bar dataKey="e" fill="#82ca9de"  onClick={()=>setCategory("e")}/>
-        <Bar dataKey="f" fill="#98fb98"  onClick={()=>setCategory("f")}/>
+        <Bar
+          style={{ cursor: "Pointer" }}
+          dataKey="a"
+          fill="#8a2be2"
+          onClick={() => setCategory("a")}
+        />
+        <Bar
+          style={{ cursor: "pointer" }}
+          dataKey="b"
+          fill="#20b2aa"
+          onClick={() => setCategory("b")}
+        />
+        <Bar
+          style={{ cursor: "pointer" }}
+          dataKey="c"
+          fill="#ff4500"
+          onClick={() => setCategory("c")}
+        />
+        <Bar
+          style={{ cursor: "pointer" }}
+          dataKey="d"
+          fill="#1e90ff"
+          onClick={() => setCategory("d")}
+        />
+        <Bar
+          style={{ cursor: "pointer" }}
+          dataKey="e"
+          fill="#82ca9de"
+          onClick={() => setCategory("e")}
+        />
+        <Bar
+          style={{ cursor: "pointer" }}
+          dataKey="f"
+          fill="#98fb98"
+          onClick={() => setCategory("f")}
+        />
       </BarChart>
     </ResponsiveContainer>
   );
